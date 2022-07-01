637bf3c31d9715b063be4e4e93dea41340fb0093
lzlzlx2000 <68079350+lzlzlx2000@users.noreply.github.com>
2022-07-02 07:36:04 +0800

    修改了页面样式

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,4 @@
 <template>
-  <nav>
-    <router-link to="/login">login</router-link>|
-    <router-link to="/register">register</router-link>
-  </nav>
   <router-view/>
 
 
--- src/views/buyer.vue
+++ src/views/buyer.vue
@@ -1,5 +1,99 @@
 <template>
-  <div>
-    采购人员
+  <div class="common-layout">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside >
+          <el-scrollbar>
+          <el-menu :default-openeds="['1']">
+            <el-sub-menu index="1">
+              <template #title>
+                <el-icon><message /></el-icon>Navigator One
+              </template>
+              <el-menu-item-group>
+                <template #title>Group 1</template>
+                <el-menu-item index="1-1">Option 1</el-menu-item>
+              </el-menu-item-group>
+            </el-sub-menu>
+          </el-menu>
+        </el-scrollbar>
+        </el-aside>
+        <el-main>
+            <el-table :data="tableData">
+              <el-table-column prop="date" label="Date" width="180" />
+              <el-table-column prop="name" label="Name" width="180" />
+              <el-table-column prop="address" label="Address" />
+            </el-table>
+          <el-pagination
+              :page-size="20"
+              :pager-count="20"
+              layout="prev, pager, next"
+              :total="1000"
+          />
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
+
+<script setup>
+const tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-08',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-06',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-07',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+]
+</script>
+
+<style>
+.el-header{
+  background-color: rgb(198,226,255);
+  margin-left: 3%;
+  margin-right: 3%;
+}
+.el-aside{
+  width:20%;
+  /*background-color:rgb(217,236,255);*/
+  margin-left: 3%;
+  height: 500px;
+}
+.el-main{
+  background-color: rgb(236,245,255);
+  margin-right: 3%;
+}
+.el-table{
+  height: 90%;
+  width: 100%;
+}
+</style>
