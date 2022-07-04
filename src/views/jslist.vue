b8fcba55dda3059a3690adacfb7fda2002cfa5d2
lzlzlx2000 <68079350+lzlzlx2000@users.noreply.github.com>
2022-07-04 16:19:03 +0800

    修改了采购员页面

--- src/views/jslist.vue
+++ src/views/jslist.vue
@@ -5,17 +5,14 @@
   >
     <el-table-column prop="isbn" label="书号"/>
     <el-table-column prop="qs_total" label="总数"/>
-    <el-table-column prop="purNum" label="采购数量">
-
-      <template inline-template v-slot="scope">
-        <el-input-number v-model="scope.row.number"
-                         @change="c(scope.$index,scope.row,$event)"
-        >
-
-        </el-input-number>
-      </template>
-
-    </el-table-column>
+      <el-table-column label="采购数量" align="center" >
+        <template inline-template v-slot="scope">
+          <el-input-number v-model="scope.row.dgTotal" :min="0" :max="999"
+                           @change="changedMatNum(scope.$index,scope.row,$event)"
+                           placeholder="请输入数量">
+          </el-input-number>
+        </template>
+      </el-table-column>
 
     <el-table-column prop="buybutton" label="操作">
       <el-button>采购</el-button>
@@ -31,8 +28,18 @@ export default {
     return {
       tableData: [{
         isbn: '456789',
-        qs_total:'8'
+        qs_total:'8',
+        dgTotal: '1'
       }]
+    }
+  },
+  methods:{
+    changedMatNum(index,row,even){
+      console.log('-------数量------');
+      console.log("index：" + index);
+      console.log("even：" + even);
+      this.tableData[index].number=even;
+      // console.log(even.currentTarget.nextElementSibling);
     }
   }
 }
