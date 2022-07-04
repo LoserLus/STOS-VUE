25ccd05bd7672748e44ca9f265c83f8237fdd115
Jack <474518361@qq.com>
2022-07-04 14:37:45 +0800

    捞版进书单（未完成）

--- src/views/jslist.vue
+++ src/views/jslist.vue
@@ -3,20 +3,19 @@
   <el-table
       :data="tableData"
   >
-    <el-table-column prop="isbn" label="书号"  />
-    <el-table-column prop="qs_total" label="总数"  />
+    <el-table-column prop="isbn" label="书号"/>
+    <el-table-column prop="qs_total" label="总数"/>
     <el-table-column prop="buybutton" label="是否已采购">
 
     </el-table-column>
 
-
-
   </el-table>
-  <el-button @click="buySome = true " >已采购</el-button>
+
+  <el-button @click="buySome = true ">已采购</el-button>
   <el-dialog v-model="buySome" title="请输入已购数量">
     <el-form :model="form">
       <el-form-item label="已购数量" :label-width="formLabelWidth">
-        <el-input v-model="form.name" autocomplete="off" />
+        <el-input v-model="form.name" autocomplete="off"/>
       </el-form-item>
     </el-form>
     <template #footer>
@@ -29,38 +28,13 @@
 
 
 </template>
-<script script lang="ts" setup>
-import { reactive, ref } from 'vue'
+<script lang="ts" setup>
+import {reactive,ref} from 'vue'
 const buySome = ref(false)
-
 const formLabelWidth = '140px'
-//
-// export default {
-//   name:"jslist",
-//   data(){
-//     return{
-//       tableData:[
-//         {
-//           isbn:'159357',
-//           qs_total:'50'
-//         }
-//       ]
-//
-//     }
-//   }
-// }
-//123
 const form = reactive({
   name: '',
-  region: '',
-  date1: '',
-  date2: '',
-  delivery: false,
-  type: [],
-  desc: '',
 })
-
-
 
 </script>
 
