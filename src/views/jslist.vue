a851da19937a04a2aebcbc62935a1204f48c6775
Jack <474518361@qq.com>
2022-07-04 15:27:48 +0800

    改进版本进书单

--- src/views/jslist.vue
+++ src/views/jslist.vue
@@ -1,42 +1,44 @@
 <template>
-  <p>进书单</p>
+  
   <el-table
       :data="tableData"
   >
     <el-table-column prop="isbn" label="书号"/>
     <el-table-column prop="qs_total" label="总数"/>
-    <el-table-column prop="buybutton" label="是否已采购">
+    <el-table-column prop="purNum" label="采购数量">
 
+      <template inline-template v-slot="scope">
+        <el-input-number v-model="scope.row.number"
+                         @change="c(scope.$index,scope.row,$event)"
+        >
+
+        </el-input-number>
+      </template>
+
+    </el-table-column>
+
+    <el-table-column prop="buybutton" label="操作">
+      <el-button>采购</el-button>
     </el-table-column>
 
   </el-table>
 
-  <el-button @click="buySome = true ">已采购</el-button>
-  <el-dialog v-model="buySome" title="请输入已购数量">
-    <el-form :model="form">
-      <el-form-item label="已购数量" :label-width="formLabelWidth">
-        <el-input v-model="form.name" autocomplete="off"/>
-      </el-form-item>
-    </el-form>
-    <template #footer>
-      <span class="dialog-footer">
-        <el-button @click="buySome = false">取消</el-button>
-        <el-button type="primary" @click="buySome = false">确定</el-button>
-      </span>
-    </template>
-  </el-dialog>
-
-
 </template>
-<script lang="ts" setup>
-import {reactive,ref} from 'vue'
-const buySome = ref(false)
-const formLabelWidth = '140px'
-const form = reactive({
-  name: '',
-})
+<script>
+export default {
+  name:'jslist',
+  data(){
+    return {
+      tableData: [{
+        isbn: '456789',
+        qs_total:'8'
+      }]
+    }
+  }
+}
 
 </script>
+
 
 <style scoped>
 
