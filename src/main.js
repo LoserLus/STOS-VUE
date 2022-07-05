3ff06f2e13c7e107a8c17de4da07a6722a02b63a
Loser_Lus <luyilong1314@126.com>
2022-07-05 10:09:51 +0800

    增加路由守护,用户未登录,无法访问管理界面

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
+import {createApp, inject} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -7,6 +7,23 @@ import axios from "axios";
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import VueAxios from 'vue-axios'
-
+import VueCookies from 'vue-cookies'
 axios.defaults.withCredentials = true;
-createApp(App).use(store).use(VueAxios,axios).use(router).use(ElementPlus).mount('#app')
+
+
+const app=createApp(App).use(store).use(VueAxios,axios).use(router).use(ElementPlus).use(VueCookies).mount('#app')
+//App.config.globalProperties.$cookies = VueCookies
+
+router.beforeEach((to,from,next)=>{
+    if(to.path == '/login' || to.path == '/register'||to.path=='/'){
+        next();
+    }else{
+        //console.log(app.$cookies.keys("session_id"));
+        if(!app.$cookies.isKey("session_id")) {
+            alert('您还没有登录，请先登录');
+            next('/login');
+        }
+        else
+            next();
+    }
+})
