e1fca354d45fee721121c9ac79209e48788d0727
lzlzlx2000 <68079350+lzlzlx2000@users.noreply.github.com>
2022-07-06 19:29:58 +0800

    实现了领书业务

--- src/views/NavMenu/stnavmenu.vue
+++ src/views/NavMenu/stnavmenu.vue
@@ -5,7 +5,7 @@
       <el-sub-menu index="1">
         <template #title>业务处理</template>
         <el-menu-item index="/student/book">订购业务</el-menu-item>
-        <el-menu-item index="/student/getbook">领书历史</el-menu-item>
+        <el-menu-item index="/student/getbook">领书业务</el-menu-item>
       </el-sub-menu>
     </el-menu>
   </div>
--- src/views/getbook.vue
+++ src/views/getbook.vue
@@ -1,13 +1,59 @@
 <template>
-<p>getbook</p>
-</template>
+  <div class="common-layout">
 
+    <el-table ref="multipleTableRef"
+              :data="tableData"
+              border style="width: 100%"
+              height='380'
+    >
+      <el-table-column prop="lsId" label="领书单号"/>
+      <el-table-column prop="dgId" label="订购单号"/>
+      <el-table-column prop="lsDate" label="领书日期"/>
+      <el-table-column prop="lsUsername" label="经办人"/>
+    </el-table>
+  </div>
+</template>
 <script>
+import axios from "axios";
+import {ref} from 'vue';
+
 export default {
-  name: "getbook"
+  name: 'getbook',
+  data() {
+    return {
+      tableData: []
+    }
+  },
+  //获取初始页面
+  created() {
+    console.log(sessionStorage);
+    const that = this;
+    //const { proxy } = getCurrentInstance();
+    axios({
+      method: 'get',
+      url: '/api/user/getText/',
+    }).then(function (response) {
+      let list = eval(response.data);
+      console.log(list.data);
+      that.tableData = list.data;
+      console.log(that.tableData);
+    })
+  }
 }
 </script>
 
-<style scoped>
+
+<style>
+
+.el-header,
+.el-footer {
+  background-color: #FFFFFF;
+  color: #333;
+  text-align: center;
+  height: 55px;
+  line-height: 80px;
+  padding: 0px;
+}
+
 
 </style>
